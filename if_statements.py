0591afa55be70784edbf9f7b5e3a62b87bbd2ed8
IrvACD <irvin.acd@gmail.com>
2021-09-14 21:40:29 -0500

    Python file that contains 'if statements' fundamentals.

---
+++ if_statements.py
@@ -0,0 +1,9 @@
+hot_day = False
+cold_day = False
+
+if hot_day:
+    print("It's a hot day. Drink plenty of water!")
+elif cold_day:
+    print("It's a cold day. Wear warm clothes.")
+else:
+    print("It's a lovely day!")
