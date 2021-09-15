de9e5556b36021fe7f8bb87de6a5274624558e97
IrvACD <irvin.acd@gmail.com>
2021-09-14 22:04:42 -0500

    Spelling correction for one of the variables.

--- house_downpayment.py
+++ house_downpayment.py
@@ -2,7 +2,7 @@ house_price = int(input("House price: "))
 good_credit = True
 
 if good_credit:
-    downpayment = house_price * 0.1
+    down_payment = house_price * 0.1
 else:
-    downpayment = house_price * 0.2
-print(f"Downpayment: ${downpayment}")
+    down_payment = house_price * 0.2
+print(f"Downpayment: ${down_payment}")
